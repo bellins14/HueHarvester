9651ac27c197baca8448a5c5f49da7d0aff85fde
Tommaso Bellinato <tommaso.bellinato@gmail.com>
2024-05-24 02:33:12 +0200

    migliorie * a posto fotocamera, si può migliorare * correggere status bar (modalità scura/chiara-land/port) * impedire reverse portrait * continuare

--- app/src/main/java/com/example/hueharvester/MainActivity.kt
+++ app/src/main/java/com/example/hueharvester/MainActivity.kt
@@ -5,24 +5,30 @@ package com.example.hueharvester
 import android.content.pm.PackageManager
 import android.hardware.Camera
 import android.Manifest
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
+import android.view.Surface
+import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 
-
 class MainActivity : AppCompatActivity() {
-    private lateinit var cameraPreview: SurfaceView
-    private lateinit var camera: Camera
-    private lateinit var tabLayout: TabLayout
+
     private lateinit var viewPager: ViewPager
-    private val cameraRequestCode = 100
+    private lateinit var tabLayout: TabLayout
+    private lateinit var cameraPreview: SurfaceView
+    private var camera: Camera? = null
+    private val CAMERA_REQUEST_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -34,7 +40,7 @@ class MainActivity : AppCompatActivity() {
         tabLayout.setupWithViewPager(viewPager, true)
 
         if (checkCameraPermission()) {
-            setupCamera()
+            setupCameraPreview()
         } else {
             requestCameraPermission()
         }
@@ -42,39 +48,85 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupViewPager(viewPager: ViewPager) {
         val adapter = ViewPagerAdapter(supportFragmentManager)
-        adapter.addFragment(RealTimeRGBFragment(), "RGB Values")
-        adapter.addFragment(LineGraphFragment(), "Line Graph")
+        adapter.addFragment(RealTimeRGBFragment(), getString(R.string.RGB_values))
+        adapter.addFragment(LineGraphFragment(), getString(R.string.line_graph))
         viewPager.adapter = adapter
     }
 
-    private fun setupCamera() {
-        camera = Camera.open()
-        val params = camera.parameters
+    private fun setupCameraPreview() {
+        val holder: SurfaceHolder = cameraPreview.holder
+        holder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                Log.d("MainActivity", "Surface created")
+                initializeCamera(holder)
+            }
 
-        // Get the supported preview sizes
-        val supportedPreviewSizes = params.supportedPreviewSizes
-        val minPreviewSize = supportedPreviewSizes.minByOrNull { it.width * it.height }
+            override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+                Log.d("MainActivity", "Surface changed")
+                releaseCamera()
+                initializeCamera(holder)
+            }
 
-        // Get the supported preview FPS ranges
-        val supportedPreviewFpsRanges = params.supportedPreviewFpsRange
-        val minPreviewFpsRange = supportedPreviewFpsRanges.minByOrNull { it[Camera.Parameters.PREVIEW_FPS_MIN_INDEX] }
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                Log.d("MainActivity", "Surface destroyed")
+                releaseCamera()
+            }
+        })
+    }
 
-        // Set the minimum preview size and frame rate
-        minPreviewSize?.let {
-            params.setPreviewSize(it.width, it.height)
+    private fun initializeCamera(holder: SurfaceHolder) {
+        try {
+            camera = Camera.open()
+            adjustCameraOrientation()
+            val params = camera?.parameters
+            val supportedPreviewSizes = params?.supportedPreviewSizes
+            val minPreviewSize = supportedPreviewSizes?.minByOrNull { it.width * it.height }
+
+            minPreviewSize?.let {
+                params.setPreviewSize(it.width, it.height)
+            }
+
+            val supportedPreviewFpsRanges = params?.supportedPreviewFpsRange
+            val minPreviewFpsRange = supportedPreviewFpsRanges?.minByOrNull { it[Camera.Parameters.PREVIEW_FPS_MIN_INDEX] }
+
+            minPreviewFpsRange?.let {
+                params.setPreviewFpsRange(it[Camera.Parameters.PREVIEW_FPS_MIN_INDEX], it[Camera.Parameters.PREVIEW_FPS_MAX_INDEX])
+            }
+
+            camera?.parameters = params
+            camera?.setPreviewDisplay(holder)
+            camera?.startPreview()
+
+            camera?.setPreviewCallback { data, camera ->
+                // TODO: Calculate average color values here
+            }
+        } catch (e: Exception) {
+            Log.e("MainActivity", "Camera setup failed", e)
+            releaseCamera()
+        }
+    }
+
+    private fun adjustCameraOrientation() {
+        val rotation = windowManager.defaultDisplay.rotation
+        var degrees = 0
+        when (rotation) {
+            Surface.ROTATION_0 -> degrees = 0
+            Surface.ROTATION_90 -> degrees = 90
+            Surface.ROTATION_180 -> degrees = 180
+            Surface.ROTATION_270 -> degrees = 270
         }
 
-        minPreviewFpsRange?.let {
-            params.setPreviewFpsRange(it[Camera.Parameters.PREVIEW_FPS_MIN_INDEX], it[Camera.Parameters.PREVIEW_FPS_MAX_INDEX])
-        }
+        val info = Camera.CameraInfo()
+        Camera.getCameraInfo(Camera.CameraInfo.CAMERA_FACING_BACK, info)
+        val result = (info.orientation - degrees + 360) % 360
+        camera?.setDisplayOrientation(result)
+    }
 
-        camera.parameters = params
-        camera.setPreviewDisplay(cameraPreview.holder)
-        camera.startPreview()
-
-        /*camera.setPreviewCallbackWithBuffer { data, camera ->
-            // TODO: Calculate average color values here
-        }*/
+    private fun releaseCamera() {
+        camera?.setPreviewCallback(null)
+        camera?.stopPreview()
+        camera?.release()
+        camera = null
     }
 
     private fun checkCameraPermission(): Boolean {
@@ -82,14 +134,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun requestCameraPermission() {
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), cameraRequestCode)
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == cameraRequestCode) {
+        if (requestCode == CAMERA_REQUEST_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                setupCamera()
+                setupCameraPreview()
             } else {
                 Toast.makeText(this, "Camera permission is required", Toast.LENGTH_SHORT).show()
             }
@@ -98,14 +150,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
-        camera.stopPreview()
-        camera.release()
+        releaseCamera()
     }
 
     override fun onResume() {
         super.onResume()
         if (checkCameraPermission()) {
-            setupCamera()
+            setupCameraPreview()
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE ||
+            newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            releaseCamera()
+            setupCameraPreview()
         }
     }
 }
